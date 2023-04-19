fb2f6ba0e3b08826411143161117f794a17f4d9a
nihal599 <66112977+nihal599@users.noreply.github.com>
2023-04-19 22:02:12 +0200

    Update htmlistener.java

--- src/htmlistener.java
+++ src/htmlistener.java
@@ -1,4 +1,65 @@
+import java.io.*;
+import java.util.Scanner;
+import org.antlr.v4.runtime.TokenStreamRewriter;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
+public class htmlistener extends JavaParserBaseListener {//color all green and nonvisited red
+    int i;
+    int c = 0, end = 0;
+    boolean exp = false;
+    TokenStreamRewriter rewriter;
+    public htmlistener(TokenStreamRewriter rewriter)  {
+        this.rewriter = rewriter;
+        this.i = 0;
+    }
+
+    @Override
+    public void enterBlock(JavaParser.BlockContext ctx) {
+        this.i++;
+        boolean f=false;
+        // Open the file
+        File file = new File("task2.txt");
+        Scanner scanner = null;//intialize scanner
+        try {// found the file to create scanner
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {// throw exception if file not found
+            throw new RuntimeException(e);
+        }
+        // Read the file contents
+        while (scanner.hasNextLine()) {//read each line in the file
+            String line = scanner.nextLine();
+            if (line.equals("block "+this.i+" is Visited")) {//check each line contain the string block {} is visted with number in {}
+                f = true;
+
+            }
+
+        }
+        scanner.reset();
+        scanner.close();
+        if(!f){//if block not found in txt file color it red
+            rewriter.insertBefore(ctx.getStart(),"<pre style=\"background-color:#FF4A4A;\">");//color block red
+            rewriter.insertAfter(ctx.getStop(),"</pre>\n");
+        }
+    }
+
+    @Override
+    public void enterCompilationUnit(JavaParser.CompilationUnitContext ctx) {//adding html tags and clor all body with green
+        String HTTMLHeader = "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "    <title>\n" +
+                "        Background\n" +
+                "    </title>\n" +
+                "    <body style=\"margin: 0%;\">\n" +
+                "        <pre style=\"background-color: rgb(181, 248, 190); margin: 0%;\">";
+        rewriter.insertBefore(ctx.getStart(), "\n" + HTTMLHeader + "\n");
+    }
+    @Override
+    public void exitCompilationUnit(JavaParser.CompilationUnitContext ctx) {
+        String HTMLFooter = "</pre>\n" +
+                "    </body>\n" +
+                "</html>";
+        rewriter.insertAfter(ctx.getStop(), "\n" + HTMLFooter + "\n");
+    }
 /// branch covrage
     @Override
     public void visitTerminal(TerminalNode node) {
